fba1af29aebf9f125453ac7ea54937712944bd87
NickWoodUK <nickwood@gmail.com>
2021-04-18 10:47:26 -0500

    Update document-creator.ts

--- document-creator.ts
+++ document-creator.ts
@@ -3,13 +3,12 @@ import { parse } from "https://deno.land/std@0.93.0/flags/mod.ts";
 const args = parse(Deno.args);
 
 if (args.url === undefined) {
-    console.error("No repo url defined");
-    Deno.exit();
+  console.error("No repo url defined");
+  Deno.exit();
 }
 
 const res = await fetch(args.url);
 
-console.log(res)
+console.log(res);
 
 //console.dir(parse(Deno.args));
-
